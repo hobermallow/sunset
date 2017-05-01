65c684835768cf18dec2cf36350f81443f8f7b1c
hobermallow <sunset@sunset.(none)>
2017-05-01 10:02:55 -0400

    aggiunto file per la topologia

---
+++ topology.tcl
@@ -0,0 +1,80 @@
+for { set i 6 } { $i < 20 } { incr i } { 
+	set position_($i) [new "WOSS/Position/WayPoint"]
+}
+$position_(1) setLatitude_ 63.481161
+$position_(1) setLongitude_ 10.538174
+$position_(1) setAltitude_ -24.366310
+
+$position_(2) setLatitude_ 63.479905
+$position_(2) setLongitude_ 10.546636
+$position_(2) setAltitude_ -115.132062
+
+$position_(3) setLatitude_ 63.481880
+$position_(3) setLongitude_ 10.550393
+$position_(3) setAltitude_ -31.705108
+
+$position_(4) setLatitude_ 63.482956
+$position_(4) setLongitude_ 10.540669
+$position_(4) setAltitude_ -177.838224
+
+$position_(5) setLatitude_ 63.484451
+$position_(5) setLongitude_ 10.548637
+$position_(5) setAltitude_ -27.523270
+
+$position_(6) setLatitude_ 63.483548
+$position_(6) setLongitude_ 10.549942
+$position_(6) setAltitude_ -88.793584
+
+$position_(7) setLatitude_ 63.485563
+$position_(7) setLongitude_ 10.540313
+$position_(7) setAltitude_ -42.776075
+
+$position_(8) setLatitude_ 63.485328
+$position_(8) setLongitude_ 10.549498
+$position_(8) setAltitude_ -58.239446
+
+$position_(9) setLatitude_ 63.486275
+$position_(9) setLongitude_ 10.555810
+$position_(9) setAltitude_ -109.915664
+
+$position_(10) setLatitude_ 63.489761
+$position_(10) setLongitude_ 10.540325
+$position_(10) setAltitude_ -164.350410
+
+$position_(11) setLatitude_ 63.489565
+$position_(11) setLongitude_ 10.544505
+$position_(11) setAltitude_ -47.780625
+
+$position_(12) setLatitude_ 63.488213
+$position_(12) setLongitude_ 10.550002
+$position_(12) setAltitude_ -109.896646
+
+$position_(13) setLatitude_ 63.493561
+$position_(13) setLongitude_ 10.538008
+$position_(13) setAltitude_ -22.490248
+
+$position_(14) setLatitude_ 63.492125
+$position_(14) setLongitude_ 10.543722
+$position_(14) setAltitude_ -70.150607
+
+$position_(15) setLatitude_ 63.493292
+$position_(15) setLongitude_ 10.552886
+$position_(15) setAltitude_ -32.106358
+
+$position_(16) setLatitude_ 63.495353
+$position_(16) setLongitude_ 10.540490
+$position_(16) setAltitude_ -33.702862
+
+$position_(17) setLatitude_ 63.496619
+$position_(17) setLongitude_ 10.547506
+$position_(17) setAltitude_ -51.905406
+
+$position_(18) setLatitude_ 63.496585
+$position_(18) setLongitude_ 10.553272
+$position_(18) setAltitude_ -162.119082
+
+$position_(19) setLatitude_ 63.481184
+$position_(19) setLongitude_ 10.538808
+$position_(19) setAltitude_ -24.162123
+
+
